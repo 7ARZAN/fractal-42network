8a1eb5c60fd5ec91233625c306a5a1597dc798a9
7ARZAN <7arzan@7ARZANs-MacBook-Air.local>
2023-01-11 11:52:31 +0100

    not normed yet *_*

--- main.c
+++ main.c
@@ -23,5 +23,53 @@ void  ft_init(t_data *data, int fractol)
       &data->image.endian);
   data->color = 0xff801;
   data->color.code = 0;
+  data->julia = (fractol == 2);
+  data->burning_ship = (fractol == 3);
+  data->julia_move = 0;
+  data->var.a_r = -0.466;
+  data->var.b_r = 0.622;
+}
 
+int fun(int x, int y, t_data *data)
+{
+  if (x >= 0 && x <= 600 && y >= 0 && y <= 600 && data->julia_move)
+  {
+    data->var.a_r = ft_map(x, data->ox, data->zoom, data->ox + data->zoom);
+    data->var.b_r = ft_map(y, data->oy, data->zoom, data->oy + data->zoom);
+    ft-draw(*data);
   }
+  return (1);
+}
+
+int main(int ac, char **av)
+{
+  t_data  data;
+  int     number;
+
+  if (ac >= 2)
+  {
+    number = ft_atoi(av[1]);
+    if (number == 1 || number == 2 || number == 3)
+    {
+      ft_instruction();
+      ft_init(&data, number);
+      ft_draw (data);
+      mlx_key_hook(data.window, ft_key_hook, &data);
+      mlx_mouse_hook(data.window, ft_mouse_hook, &data);
+      mlx_hook(data,window, 6, 0, fun, &data);
+      mlx_loop(data.cnx);
+    }
+  }
+  write(1, "##############  'Fractols'  ###############\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "# 1 - 'Mandelbrot'                        #\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "# 2 - 'Julia'                             #\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "# 3 - 'burning_ship'                      #\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "#                                         #\n", 44);
+  write(1, "##############  'Fractols'  ###############\n", 44);
+  return (0);
+}
